bc8627cf8cc17e5478bee4b4f3a3d6ff9215822c
brauni <brauni@172.25.1.216>
2017-10-17 15:50:27 +0200

    Bisherige Projekte

--- Berechnungen/Berechnungen.c
+++ Berechnungen/Berechnungen.c
@@ -1,14 +1,21 @@
-/*Berechnungen*/
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * Berechnungen.c
+ *
+ *  Created on: 13.10.2017
+ *      Author: brauni
+ */
+
+#include<stdio.h>
+
 
 int main(void)
 {
-	setbuf(stdout, NULL); // Console Fix
+	setbuf(stdout, NULL); //Console Fix
+	// Int Variablen erstellen und definieren
 	int a = 10;
 	int b = 20;
 	int c = 30;
 	int ergebnis = a*b*c;
-	printf("Das Ergebnis von a*b*c ist: %i\n", ergebnis);
+	printf("a*b*c ergibt: %i\n", ergebnis);
 	return 0;
 }
