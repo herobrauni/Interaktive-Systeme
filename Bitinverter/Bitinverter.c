bed2181780fc3e7c21e2da3771fd4ff2d6c51b49
brauni <braunnicolaj@gmail.com>
2017-10-26 21:45:48 +0200

    Added Comments

--- Bitinverter/Bitinverter.c
+++ Bitinverter/Bitinverter.c
@@ -11,16 +11,20 @@
 int main(void)
 {
 	setbuf(stdout, NULL); //Console Fix
+	//Create Variables
 	int a;
 	unsigned int n;
 	int ergebnis_positiv;
 	int ergebnis_negativ;
+	//Define Variables by Userinput
 	printf("Bitte geben sie eine ganze Zahl \"a\" ein: ");
 	scanf("%i", &a);
 	printf("Bitte geben sie eine natürliche Zahl \"n\" ein: ");
 	scanf("%i", &n);
+	//Bitverschiebungen
 	ergebnis_positiv= a <<n;
 	ergebnis_negativ= a >>n;
+	//Ausgabe der Ergebnisse
 	printf("a*2^n = %i*2^%i = %i\n", a, n, ergebnis_positiv );
 	printf("a*2^-n = %i*2^-%i = %i", a, n, ergebnis_negativ);
 	return 0;
